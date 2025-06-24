bb68156d0ae8f15e85b5b1eac83de064c23bfcac
Erak0 <koepkaeryk@gmail.com>
2025-06-24 13:51:25 +0200

    all

---
+++ build/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/eryk/projekt/PO-projekt/Buttons.cpp" "CMakeFiles/Game.dir/Buttons.cpp.o" "gcc" "CMakeFiles/Game.dir/Buttons.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Camera.cpp" "CMakeFiles/Game.dir/Camera.cpp.o" "gcc" "CMakeFiles/Game.dir/Camera.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Enemy.cpp" "CMakeFiles/Game.dir/Enemy.cpp.o" "gcc" "CMakeFiles/Game.dir/Enemy.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/EnemyManager.cpp" "CMakeFiles/Game.dir/EnemyManager.cpp.o" "gcc" "CMakeFiles/Game.dir/EnemyManager.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/GameState.cpp" "CMakeFiles/Game.dir/GameState.cpp.o" "gcc" "CMakeFiles/Game.dir/GameState.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Init.cpp" "CMakeFiles/Game.dir/Init.cpp.o" "gcc" "CMakeFiles/Game.dir/Init.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Map.cpp" "CMakeFiles/Game.dir/Map.cpp.o" "gcc" "CMakeFiles/Game.dir/Map.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/OtherFunctions.cpp" "CMakeFiles/Game.dir/OtherFunctions.cpp.o" "gcc" "CMakeFiles/Game.dir/OtherFunctions.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Player.cpp" "CMakeFiles/Game.dir/Player.cpp.o" "gcc" "CMakeFiles/Game.dir/Player.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/PlayerEventHandling.cpp" "CMakeFiles/Game.dir/PlayerEventHandling.cpp.o" "gcc" "CMakeFiles/Game.dir/PlayerEventHandling.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/Settings.cpp" "CMakeFiles/Game.dir/Settings.cpp.o" "gcc" "CMakeFiles/Game.dir/Settings.cpp.o.d"
+  "/home/eryk/projekt/PO-projekt/main.cpp" "CMakeFiles/Game.dir/main.cpp.o" "gcc" "CMakeFiles/Game.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/Buttons.cpp.o"
+  "CMakeFiles/Game.dir/Buttons.cpp.o.d"
+  "CMakeFiles/Game.dir/Camera.cpp.o"
+  "CMakeFiles/Game.dir/Camera.cpp.o.d"
+  "CMakeFiles/Game.dir/Enemy.cpp.o"
+  "CMakeFiles/Game.dir/Enemy.cpp.o.d"
+  "CMakeFiles/Game.dir/EnemyManager.cpp.o"
+  "CMakeFiles/Game.dir/EnemyManager.cpp.o.d"
+  "CMakeFiles/Game.dir/GameState.cpp.o"
+  "CMakeFiles/Game.dir/GameState.cpp.o.d"
+  "CMakeFiles/Game.dir/Init.cpp.o"
+  "CMakeFiles/Game.dir/Init.cpp.o.d"
+  "CMakeFiles/Game.dir/Map.cpp.o"
+  "CMakeFiles/Game.dir/Map.cpp.o.d"
+  "CMakeFiles/Game.dir/OtherFunctions.cpp.o"
+  "CMakeFiles/Game.dir/OtherFunctions.cpp.o.d"
+  "CMakeFiles/Game.dir/Player.cpp.o"
+  "CMakeFiles/Game.dir/Player.cpp.o.d"
+  "CMakeFiles/Game.dir/PlayerEventHandling.cpp.o"
+  "CMakeFiles/Game.dir/PlayerEventHandling.cpp.o.d"
+  "CMakeFiles/Game.dir/Settings.cpp.o"
+  "CMakeFiles/Game.dir/Settings.cpp.o.d"
+  "CMakeFiles/Game.dir/main.cpp.o"
+  "CMakeFiles/Game.dir/main.cpp.o.d"
+  "Game"
+  "Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
